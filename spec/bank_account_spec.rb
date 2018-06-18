995b38e6cc06c35e555b2a0c424bbf0551c7ff35
jessica stenning <jessicastenning@hotmail.co.uk>
2018-06-18 14:35:25 +0100

    Adds deposit functionality to increase balance

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -7,5 +7,12 @@ describe Bank_account do
       expect(subject.balance).to eq 0
     end
   end
-  
+
+  describe '#deposit' do
+    it 'should allow the user to deposit funds' do
+      subject.deposit(10)
+      expect(subject.balance).to eq 10
+    end
+  end
+
 end
