0c848620bb4242c99bc165ebf7dbf9b9ea4684a0
jessica stenning <jessicastenning@hotmail.co.uk>
2018-06-19 09:13:07 +0100

    Updating subject in tests to bank_account

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -5,32 +5,32 @@ describe BankAccount do
 
   describe '#balance' do
     it 'should have a default balance of zero' do
-      expect(subject.balance).to eq 0
+      expect(bank_account.balance).to eq 0
     end
   end
 
   describe '#deposit' do
     it 'should allow the user to deposit funds' do
-      subject.deposit(10)
-      expect(subject.balance).to eq 10
+      bank_account.deposit(10)
+      expect(bank_account.balance).to eq 10
     end
   end
 
   describe '#withdraw' do
     it 'should allow users to withdraw funds' do
-      subject.deposit(15)
-      subject.withdraw(10)
-      expect(subject.balance).to eq 5
+      bank_account.deposit(15)
+      bank_account.withdraw(10)
+      expect(bank_account.balance).to eq 5
     end
     it 'should raise an error if a user tries to withdraw an amount greater than the balance' do
-      expect{ subject.withdraw(10) }.to raise_error "Insufficient funds"
+      expect{ bank_account.withdraw(10) }.to raise_error "Insufficient funds"
     end
   end
 
   describe '#current_transaction' do
     it 'should record details of the current deposit transaction' do
-      subject.deposit(10)
-      expect(subject.current_transaction).to eq (["10, #{BankAccount::TIME}, 10"])
+      bank_account.deposit(10)
+      expect(bank_account.current_transaction).to eq (["10, #{BankAccount::TIME}, 10"])
     end
   end
 
