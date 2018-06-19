1178872a971860488c0b775588c30ffae9f66cf4
jessica stenning <jessicastenning@hotmail.co.uk>
2018-06-19 19:19:10 +0100

    Refactor time to remove constant

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -2,28 +2,27 @@ class Transactions
   attr_reader :transactions, :balance
 
   DEFAULT_BALANCE = 0
-  TIME = Time.now.strftime('%d/%m/%y %H:%M')
 
   def initialize
     @balance = DEFAULT_BALANCE
     @transactions = []
   end
 
-  def deposit(amount)
+  def deposit(type, amount)
     balance_increase(amount)
-    confirm_transaction(amount)
+    confirm_transaction(type, amount)
   end
 
-  def withdraw(amount)
+  def withdraw(type, amount)
     raise 'Insufficient funds' if @balance < amount
     balance_decrease(amount)
-    confirm_transaction(amount)
+    confirm_transaction(type, amount)
   end
 
   private
 
-  def confirm_transaction(amount)
-    @transactions.push("#{amount}, #{TIME}, #{@balance}")
+  def confirm_transaction(type, amount)
+    @transactions.push("#{type}, #{amount}, #{Time.now.strftime('%d/%m/%y')}, #{@balance}")
   end
 
   def balance_increase(amount)
--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -11,31 +11,31 @@ describe Transactions do
 
   describe '#withdraw' do
     it 'should allow users to withdraw funds' do
-      transaction.deposit(15)
-      transaction.withdraw(10)
+      transaction.deposit("credit", 15)
+      transaction.withdraw("debit", 10)
       expect(transaction.balance).to eq 5
     end
     it 'should raise an error if withdrawal amount is greater than balance' do
-      expect { transaction.withdraw(10) }.to raise_error 'Insufficient funds'
+      expect { transaction.withdraw("debit", 10) }.to raise_error 'Insufficient funds'
     end
   end
 
   describe '#deposit' do
     it 'should allow the user to deposit funds' do
-      transaction.deposit(10)
+      transaction.deposit("credit", 10)
       expect(transaction.balance).to eq 10
     end
   end
 
   describe '#transactions' do
     it 'should record details of a deposit transaction' do
-      transaction.deposit(10)
-      expect(transaction.transactions).to include("10, #{Transactions::TIME}, 10")
+      transaction.deposit("credit", 10)
+      expect(transaction.transactions).to include("credit, 10, #{Time.now.strftime('%d/%m/%y')}, 10")
     end
     it 'should record details of a withdrawal transaction' do
-      transaction.deposit(10)
-      transaction.withdraw(8)
-      expect(transaction.transactions).to include("8, #{Transactions::TIME}, 2")
+      transaction.deposit("credit", 10)
+      transaction.withdraw("debit", 8)
+      expect(transaction.transactions).to include("debit, 8, #{Time.now.strftime('%d/%m/%y')}, 2")
     end
   end
 end
