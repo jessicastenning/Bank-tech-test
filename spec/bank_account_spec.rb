130de6682dcec959f9b43f0391f8bbe2d26abb00
jessica stenning <jessicastenning@hotmail.co.uk>
2018-06-20 15:14:15 +0100

    Adds unit tests for print_statement method

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -26,4 +26,20 @@ describe BankAccount do
       expect(transactions).to have_received(:withdraw)
     end
   end
+
+  describe '#print_statement' do
+    before do
+      allow(statement).to receive(:pretty_print)
+      allow(transactions).to receive(:transaction_history)
+    end
+
+    it 'should call the pretty_print method on statements' do
+      bank_account.print_statement
+      expect(statement).to have_received(:pretty_print)
+    end
+    it 'should call the transaction_history method on transactions' do
+      bank_account.print_statement
+      expect(transactions).to have_received(:transaction_history)
+    end
+  end
 end
